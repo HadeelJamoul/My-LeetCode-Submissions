6840cabbdc2269e345e3a32343a71c8ecfededb7
Hadeel Jamoul <84832793+HadeelJamoul@users.noreply.github.com>
2024-10-31 21:45:11 +0300

    Time: 2 ms (50%), Space: 150.6 MB (18.75%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.dart
+++ 0461-hamming-distance/0461-hamming-distance.dart
@@ -1,12 +1,7 @@
 class Solution {
   int hammingDistance(int x, int y) {
-    x.toRadixString(2).padLeft(4, '0');
-    y.toRadixString(2).padLeft(4, '0');
     String xor = (x ^ y).toRadixString(2).padLeft(4, '0');
-    print(xor);
-    print(xor.split(''));
     int res = xor.split('').where((char) => char == '1').length;
-    print(res);
     return res;
   }
 }
