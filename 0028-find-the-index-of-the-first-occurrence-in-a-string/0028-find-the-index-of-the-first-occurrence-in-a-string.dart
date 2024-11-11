4f4bc0e0dc61a4ef019c21c908f31893b90bf241
Hadeel Jamoul <84832793+HadeelJamoul@users.noreply.github.com>
2024-11-11 11:21:55 +0300

    Time: 0 ms (100%), Space: 148.2 MB (63.05%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.dart
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.dart
@@ -1,16 +1,22 @@
+//Slide a window
 class Solution {
   int strStr(String haystack, String needle) {
-    if (needle.isEmpty) return 0; // Edge case: empty needle
-    if (needle.length > haystack.length) return -1; // Needle can't fit
-
-    // Loop through `haystack` with a sliding window of `needle.length`
-    for (int i = 0; i <= haystack.length - needle.length; i++) {
-      // Check if substring matches `needle`
+    if (needle.isEmpty) return 0;
+    if (needle.length > haystack.length) return -1;
+    int limit = haystack.length - needle.length; // 11 - 5 = 6 => limit = 6
+    for (int i = 0; i <= limit; ++i) {
+      //in substring the end is optional and it gives The position just after the last character
+      //((0 , 0+5) = missi is it equal to issip)
+      //((1 , 6) = issis)
+      //((2 , 7) = ssiss)
+      //((3 , 8) = sissi)
+      //((4 , 9) = issip)
+      //((5, 10) = ssipp)
+      //((6, 11) = sippi) .
       if (haystack.substring(i, i + needle.length) == needle) {
-        return i; // Found the first occurrence
+        return i;
       }
     }
-
-    return -1; // No match found
+    return -1;
   }
 }
