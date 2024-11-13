8a80f8f976bc8795c089a6405a83fc05fcf9e679
Hadeel Jamoul <84832793+HadeelJamoul@users.noreply.github.com>
2024-11-13 08:34:12 +0300

    Time: 4 ms (6.74%), Space: 149.2 MB (58.37%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.dart
@@ -0,0 +1,21 @@
+class Solution {
+  int removeElement(List<int> nums, int val) {
+    int k = 0;
+    for (int i = 0; i < nums.length; ++i) {
+      // nums[0] => 3 continue
+      // nums[1] => 2 != 3
+      // nums[2] => 2 != 3
+      // nums[3] => 3 continue
+      if (nums[i] != val) {
+        //nums[0] = nums[1]
+        //nums[1] = nums[2]
+        nums[k] = nums[i];
+        //k = 1
+        //k = 2
+        k = k + 1;
+      }
+    }
+    print(nums);
+    return k;
+  }
+}
