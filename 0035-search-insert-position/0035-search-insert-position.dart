18877348504fd95e3d80ddd2bda80c4d4f97003d
Hadeel Jamoul <84832793+HadeelJamoul@users.noreply.github.com>
2024-11-14 09:15:05 +0300

    Time: 14 ms (7.06%), Space: 150.6 MB (36.86%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.dart
+++ 0035-search-insert-position/0035-search-insert-position.dart
@@ -1,6 +1,5 @@
 class Solution {
   int searchInsert(List<int> nums, int target) {
-    if (nums.contains(target)) return nums.indexOf(target);
     if (!nums.contains(target)) {
       nums.add(target);
       nums.sort();
