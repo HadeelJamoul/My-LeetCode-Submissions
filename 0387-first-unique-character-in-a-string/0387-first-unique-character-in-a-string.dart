93094eb9a0d6f87e4f92cc319d16e8a644228a48
Hadeel Jamoul <84832793+HadeelJamoul@users.noreply.github.com>
2024-12-09 12:50:56 +0300

    Time: 48 ms (54.55%), Space: 152 MB (27.42%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.dart
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.dart
@@ -4,9 +4,9 @@ class Solution {
     for (int i = 0; i < s.length; ++i) {
       map[s[i]] = (map[s[i]] ?? 0) + 1;
     }
-    for (String char in s.split("")) {
-      if (map[char] == 1) {
-        return s.indexOf(char);
+    for (int i = 0; i < s.length; ++i) {
+      if (map[s[i]] == 1) {
+        return i;
       }
     }
     return -1;
