37802b2a9dba54bca5a8cbb1dac1251e4170119b
Hadeel Jamoul <84832793+HadeelJamoul@users.noreply.github.com>
2024-12-19 12:32:27 +0300

    Time: 8 ms (34.48%), Space: 15.9 MB (82.18%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.swift
@@ -0,0 +1,10 @@
+class Solution {
+    func reverseBits(_ n: Int) -> Int {
+        var result = 0
+        for i in 0..<32{
+            let bit = (n >> i) & 1
+            result = result | (bit << (31 - i))
+        }
+        return result
+    }
+}
