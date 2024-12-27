4ec4d782b1752ad9e38fbf43dc1bfd321249bc8c
Hadeel Jamoul <84832793+HadeelJamoul@users.noreply.github.com>
2024-12-27 18:07:37 +0300

    Time: 0 ms (100%), Space: 147.2 MB (95.45%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.dart
@@ -0,0 +1,38 @@
+class Solution {
+  int jump(List<int> nums) {
+    int res = 0;
+    int l = 0;
+    int r = 0;
+
+    // r = right most value of our current portion
+    // applying DFS on every portion
+
+    // r = 0
+    // r = 2
+    // r = 4
+
+    // STOP r = 4
+    while (r < nums.length - 1) {
+      int farthest = 0;
+      for (int i = l; i < r + 1; ++i) {
+        // i = 0 , nums[0] = 2
+        // farthest = max(0, 2) => farthest = 2
+
+        // i = 1 , nums[1] = 3
+        // farthest = max(2, 4) => farthest = 4
+
+        farthest = max(farthest, i + nums[i]);
+      }
+      // res = 1
+      // res = 2
+      res += 1;
+      // l = 1
+      // l = 3
+      l = r + 1;
+      // r = 2
+      // r = 4
+      r = farthest;
+    }
+    return res;
+  }
+}
