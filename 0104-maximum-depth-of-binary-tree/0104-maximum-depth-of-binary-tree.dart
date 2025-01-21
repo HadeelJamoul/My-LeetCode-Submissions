bfd60101e94bf22346a8102eda34979acfd8014e
Hadeel Jamoul <84832793+HadeelJamoul@users.noreply.github.com>
2025-01-21 09:34:13 +0300

    Time: 0 ms (100%), Space: 150.7 MB (35%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.dart
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *   int val;
+ *   TreeNode? left;
+ *   TreeNode? right;
+ *   TreeNode([this.val = 0, this.left, this.right]);
+ * }
+ */
+class Solution {
+  int maxDepth(TreeNode? root) {
+    if (root == null) {
+      return 0;
+    }
+
+    int right = maxDepth(root.right);
+    int left = maxDepth(root.left);
+
+    return 1 + max(right, left);
+  }
+}
