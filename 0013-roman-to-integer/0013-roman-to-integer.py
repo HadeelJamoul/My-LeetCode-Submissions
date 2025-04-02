f17094e06b45a29c33259874e2628a63d9fb9a80
Hadeel Jamoul <84832793+HadeelJamoul@users.noreply.github.com>
2025-04-02 19:23:30 +0300

    Time: 3 ms (79.95%), Space: 18 MB (21.16%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,32 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        sum = 0
+        i = 0
+
+        singleSymbol = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        pairSymbol = {
+            'IV': 4,
+            'IX': 9,
+            'XL': 40,
+            'XC': 90,
+            'CD': 400,
+            'CM': 900,
+        }
+        
+        while(i < len(s)):
+            if s[i:i+2] in pairSymbol:
+                sum = sum + pairSymbol[s[i:i+2]]
+                i += 2
+            else:
+                sum = sum + singleSymbol[s[i]]
+                i += 1
+        return sum
