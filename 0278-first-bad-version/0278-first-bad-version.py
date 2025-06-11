e86512a5ac6c17bb198a25e8fc7cd7314878ea78
Hadeel Jamoul <84832793+HadeelJamoul@users.noreply.github.com>
2025-06-11 11:25:24 +0300

    Time: 40 ms (38.04%), Space: 17.9 MB (27.21%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -3,15 +3,22 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        l = 1
-        r = n
-        # Each iteration shrinks the range (l, r) until they meet.
-        while l < r :
-            mid = (l + r) // 2
+        left = 1
+        right = n
+        # left = 4 and right = 4
+        # so we break the loop 
+        # we can return right or left because when we execute the loop they definitely 
+        # will be the same
+        while(left < right):
+            # (1 + 5) // 2 = 3
+            # (4 + 5) // 2 = 4
+            mid = (left + right) // 2
+            # 3 is false
+            # 4 is false
             if(isBadVersion(mid)):
-                r = mid
+                # right = 4
+                right = mid
             else:
-                l = mid + 1
-        
-        # Since l and r have converged
-        return l
+                # left = 3 + 1
+                left = mid + 1
+        return left
