f45fc854dd9f76d4a71795e8b09d162e39feffe9
Hadeel Jamoul <84832793+HadeelJamoul@users.noreply.github.com>
2025-07-02 11:03:41 +0300

    Time: 0 ms (100%), Space: 17.7 MB (81.39%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -1,3 +1,4 @@
+# Definition for a binary tree node.
 from typing import Optional
 
 
@@ -21,3 +22,20 @@ class Solution:
         self.invertTree(root.left)
 
         return root
+
+# print tree
+def print_pre_order(root):
+    if root == None:
+        return None
+    
+    print(root.val , end = " ")
+    print_pre_order(root.left)
+    print_pre_order(root.right)
+    
+
+# Time Complexity: o(n)
+# space Complexity: must of the cases in tree search we are storing the nodes on the way down 
+# So it is o(h) while h the hight of the tree
+
+inverted = Solution().invertTree(TreeNode(2, TreeNode(1), TreeNode(3)))
+print_pre_order(inverted)
