46c52aed9bd9967d9cd526b0b7e0299d237cf8b2
Hadeel Jamoul <84832793+HadeelJamoul@users.noreply.github.com>
2025-08-10 10:03:45 +0300

    Time: 51 ms (88.81%), Space: 23.1 MB (21.52%) - LeetHub

---
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -0,0 +1,22 @@
+from typing import List
+import heapq
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        def count_dis(x, y):
+            return (x*x)+(y*y)
+        
+        heap = []
+        # at the top of the heap we have
+        # the minimum value => the most negative => the farthest
+        # so all k point in the heap are required in the answer
+        for x, y in points:
+            d = count_dis(x,y)
+            if (len(heap) < k):
+                heapq.heappush(heap, (-d , x, y))
+            else:
+                heapq.heappushpop(heap, (-d, x, y))
+            
+        return [(x,y) for d, x, y in heap]
+
+print(Solution().kClosest([[1,3],[-2,2]], 1))
